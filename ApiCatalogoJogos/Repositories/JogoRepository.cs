87370c305eebe230507cbecc3b0af1d0b50a5932
Vanessa Furtado de Lima <vanessalima@MacBook-Pro-de-Vanessa.local>
2022-05-10 09:44:26 -0300

    updating api project

---
+++ ApiCatalogoJogos/Repositories/JogoRepository.cs
@@ -0,0 +1,67 @@
+﻿using ApiCatalogoJogos.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.Repositories
+{
+    public class JogoRepository : IJogoRepository
+    {
+        private static Dictionary<Guid, Jogo> jogos = new Dictionary<Guid, Jogo>()
+        {
+            {Guid.Parse("0ca314a5-9282-45d8-92c3-2985f2a9fd04"), new Jogo {Id = Guid.Parse("0ca314a5-9282-45d8-92c3-2985f2a9fd04"), Nome = "Fifa 21", Produtora = "EA", Preco = 200} },
+            {Guid.Parse("0ca314a5-9282-45d8-92c3-2985f2a9fd05"), new Jogo {Id = Guid.Parse("0ca314a5-9282-45d8-92c3-2985f2a9fd05"), Nome = "Fifa 20", Produtora = "EA", Preco = 190} }
+        };
+        public Task<List<Jogo>> Obter(int pagina, int quantidade)
+        {
+            return Task.FromResult(jogos.Values.Skip((pagina - 1) * quantidade).Take(quantidade).ToList())  ;
+        }
+        public Task<Jogo> Obter(Guid id)
+        {
+            if (!jogos.ContainsKey(id))
+                return null;
+
+            return Task.FromResult(jogos[id]);
+        }
+        public Task<List<Jogo>> Obter(string nome, string produtora)
+        {
+            return Task.FromResult(jogos.Values.Where(jogo => jogo.Nome.Equals(nome) && jogo.Produtora.Equals(produtora)).ToList());
+        }
+        public Task<List<Jogo>> ObterSemLambda(string nome, string produtora)
+        {
+            var retorno = new List<Jogo>();
+            foreach (var jogo in jogos.Values)
+            {
+                if (jogo.Nome.Equals(nome) && jogo.Produtora.Equals(produtora))
+                    retorno.Add(jogo);
+            }
+            return Task.FromResult(retorno);
+        }
+        public Task Inserir(Jogo jogo)
+        {
+            jogos.Add(jogo.Id, jogo);
+            return Task.CompletedTask;
+        }
+        public Task Atualizar(Jogo jogo)
+        {
+            jogos[jogo.Id] = jogo;
+            return Task.CompletedTask;
+        }
+        public Task Remover(Guid id)
+        {
+            jogos.Remove(id);
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            //Fechar conexão com o banco
+        }
+
+
+        
+
+        
+    }
+}
